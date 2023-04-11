f39a036b40716a67063a2402e74f9a3a2280e2ae
Igor Ilić <ilicigor@outlook.com>
2023-04-11 15:50:45 +0200

    chore: added tailwind support

---
+++ packages/truckmonitor/remix.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  ignoredRouteFiles: ['**/.*'],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  watchPaths: ['../../libs'],
+  future: {
+    unstable_postcss: true,
+    unstable_tailwind: true
+  }
+}
--- packages/truckmonitor/tailwind.config.js
+++ packages/truckmonitor/tailwind.config.js
@@ -2,11 +2,11 @@
 const {createGlobPatternsForDependencies} = require('@nrwl/react/tailwind')
 const {join} = require('path')
 
-module.exports = {
+export default {
   content: [
     join(
       __dirname,
-      '{src,pages,components}/**/*!(*.stories|*.spec|*.test).{js,jsx,ts,tsx,mdx,html}'
+      '{src,pages,components,app}/**/*!(*.stories|*.spec|*.test).{js,jsx,ts,tsx,mdx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname)
   ],
